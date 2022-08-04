69722c8bef3abfb0a0a85f6544e7fde87fbdb2fc
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-04 16:20:30 -0700

    add head to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import Dashboard from 'components/Dashboard'
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
-  return <Dashboard />
+  return (
+    <>
+      <Head>
+        <title>Jäger</title>
+      </Head>
+      <Dashboard />
+    </>
+  )
 }
 
 export default Home
