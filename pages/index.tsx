48a9cffd264ce248e9f5a3c7ad196accb38a2b2a
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-04 21:37:33 -0700

    add open graph image

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,10 @@ const Home = () => {
     <>
       <Head>
         <title>Jäger</title>
+        <meta property="og:image" content="/share.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="759" />
+        <meta property="og:image:height" content="485" />
       </Head>
       <Dashboard>
         <Stats>
--- pages/recruiters.tsx
+++ pages/recruiters.tsx
@@ -7,6 +7,10 @@ const Recruiters = () => {
     <>
       <Head>
         <title>Jäger</title>
+        <meta property="og:image" content="/share.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="759" />
+        <meta property="og:image:height" content="485" />
       </Head>
       <Dashboard></Dashboard>
     </>
