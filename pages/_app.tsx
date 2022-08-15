2bc4b7d405b26f838aadd1dcc47274498ac05787
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-14 22:39:25 -0700

    ditch google analytics for plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,16 @@ import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
+import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="jager.vercel.app">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
